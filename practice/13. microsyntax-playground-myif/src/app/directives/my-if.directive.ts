d35b5ab5dbc440b39f7deb73d0e1cf03f19f67c4
Henrik E. Rasmussen <15649333+herasmussen@users.noreply.github.com>
2026-06-29 00:52:48 +0200

    feat: enhance my-if directive with type safety and context handling

--- practice/13. microsyntax-playground-myif/src/app/directives/my-if.directive.ts
+++ practice/13. microsyntax-playground-myif/src/app/directives/my-if.directive.ts
@@ -1,17 +1,42 @@
-import { Directive, input } from "@angular/core";
+import { computed, Directive, effect, inject, input, TemplateRef, ViewContainerRef } from "@angular/core";
+
+type MyIfTruthy<T> = Exclude<T, false | 0 | '' | null | undefined>;
 
 export interface MyIfContext<T> {
-    readonly myIf: T;
+    readonly myIf: MyIfTruthy<T>;
 }
 
 @Directive({
     selector: '[myIf]'
 })
 export class MyIf<T> {
+    readonly template = inject<TemplateRef<MyIfContext<T>>>(TemplateRef);
+    readonly vcr = inject(ViewContainerRef);
     readonly myIf = input.required<T>();
+    readonly condition = computed(() => !!this.myIf());
+
+    invalidate() {
+        const condition = this.condition();
+
+        if (condition) {
+            this.vcr.clear();
+            const ctx: MyIfContext<T> = { myIf: this.myIf() as MyIfTruthy<T> };
+            this.vcr.createEmbeddedView(this.template, ctx);
+        }
+        else {
+            if (this.vcr.length > 0) {
+                this.vcr.clear();
+            }
+        }
+    }
+
+    constructor() {
+        effect(() => {
+            this.invalidate();
+        });
+    }
 
     static ngTemplateContextGuard<T>(_: MyIf<T>, ctx: unknown): ctx is MyIfContext<T> {
         return true;
     }
-
 }
